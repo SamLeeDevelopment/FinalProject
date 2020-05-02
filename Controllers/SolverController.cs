c8c6b86ea2368806cbdaa7ba46c5fed2eb09530f
NickChervek <nickchervek@gmail.com>
2020-05-02 11:58:06 -0500

    New Version

--- Controllers/SolverController.cs
+++ Controllers/SolverController.cs
@@ -2,34 +2,108 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Identity;
 using System.Security.Claims;
-using Final_Project.Algorithm;
 using System;
 using System.Xml.Linq;
+using QuizletAnswerFinder.AnswerFinding;
+using QuizletAnswerFinder.Models;
+using QuizletAnswerFinder;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Final_Project.Data;
+using System.Linq;
+using Microsoft.EntityFrameworkCore.Query.Internal;
+using Microsoft.EntityFrameworkCore;
 
 namespace Final_Project.Controllers
 {
     public class SolverController : Controller
     {
-        public AnswerFinder _answerFinder;
-
-        public SolverController ()
+        AnswerFinder _answerFinder;
+        ApplicationDbContext dbContext;
+        public SolverController(ApplicationDbContext applicationDbContext)
         {
-            this._answerFinder = new AnswerFinder();
+            _answerFinder = new AnswerFinder();
+            dbContext = applicationDbContext;
         }
 
         [HttpPost]
-        public IActionResult Query(Query model)
+        public IActionResult Query(UserEnterBindingModel model)
         {
+
+
+            //Check if subject already exist in the database
+            SubjectModel subjectModel = dbContext.Subjects.Where(s => s.Subject.Equals(model.Subject)).Include(x => x.MatchingQuerys).FirstOrDefault();
+
+            if (subjectModel != null)
+            {
+
+                if (subjectModel.MatchingQuerys != null)
+                {
+                    Query foundQueryFromSubject = subjectModel.MatchingQuerys.Where(q => q.Question.Equals(model.Question)).FirstOrDefault();
+
+                    //Found the question. Display it.
+
+                    if (foundQueryFromSubject != null)
+                    {
+                        return View(foundQueryFromSubject);
+                    }
+                }
+
+
+            }
+            else
+            {
+                //Save the new subject in the database
+                dbContext.InsertSubject(new SubjectModel { Subject = model.Subject });
+            }
+
+            //The subject exists. The query does not.
+
+            Query query = new Query();
+
+            SubjectModel subjectForQuery = dbContext.Subjects.Where(s => s.Subject.Equals(model.Subject)).First();
+
+
             //match the question asked to the user logged in
-            model.UserId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+            query.UserId = User.FindFirstValue(ClaimTypes.NameIdentifier);
 
-            //get the response from algorithm (xml)
-            model.Response = _answerFinder.FindAnswers(model.Question, "sql");
+            //Add the subject id to the query
+            //   query.SubjectModelId = subjectForQuery.ID;
 
-            //parse xml into string to be displayed
-            model.Answer = model.Response.Split("<Answer>")[1].Split("</Answer>")[0];
 
-            return View(model);
+            //get the response from algorithm
+            List<QuestionInfo> res = _answerFinder.FindAnswers(model.Question, model.Subject);
+
+            //Convert the response into a readable format
+            StringBuilder stringBuilder = new StringBuilder();
+            const String newLine = "<br>";
+            for (int i = 0; i < res.Count; i++)
+            {
+                if (i == 1)
+                {
+                    query.IsMultipleAnswer = true;
+                }
+                stringBuilder.Append(newLine);
+                stringBuilder.Append(res[i].Question);
+                stringBuilder.Append(newLine);
+                stringBuilder.Append(res[i].Answer);
+
+            }
+
+            query.Question = model.Question;
+            query.Answer = stringBuilder.ToString();
+
+
+
+
+            subjectForQuery.MatchingQuerys.Add(query);
+
+            dbContext.SaveChanges();
+
+            return View(query);
+
         }
+
     }
 }
--- Models/Query.cs
+++ Models/Query.cs
@@ -2,15 +2,24 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-
+using System.ComponentModel.DataAnnotations;
 namespace Final_Project.Models
 {
     public class Query
     {
         public int Id { get; set; }
+        public int SubjectModelId { get; set; }
+        public SubjectModel Parent { get; set; }
+
         public string UserId { get; set; }
+
         public string Question { get; set; }
-        public String Response { get; set; }
+
         public string Answer { get; set; }
+
+        public bool IsMultipleAnswer { get; set; }
+
+        public String Response { get; set; }
+
     }
 }
